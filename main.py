2e54877443c81156781f45efbffe283e88b4b1d3
faris771 <farisabufahra33@gmail.com>
2023-05-18 19:41:09 +0300

    new edit

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+#new edit
+
 # This is a sample Python script.
+
+
+
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -12,5 +18,3 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
