17b0894dac4f25161817dee471d8a294d045ecc3
faris771 <farisabufahra33@gmail.com>
2023-05-22 10:23:35 +0300

    Part2 Done✔ still need to check Dr. Abdelkarim to check the output

--- Part2/client.py
+++ Part2/client.py
@@ -1,36 +1,26 @@
 import socket
 import time
-
 from colors import Colors
 
-HOST_IP = socket.gethostbyname(socket.gethostname())  # IP of the server, this case my PC is both server and client
-PORT = 8855
-ADDRESS = (HOST_IP, PORT)  # tuple
-HEADER = 64  # HEADER represnets 64 Bytes, maximum int size that the client will send
+HOST_IP = socket.gethostbyname(socket.gethostname())  # my IP add 255 instead of last ocatate
+hostlist = HOST_IP.split('.')
+hostlist[-1] = '255'
+HOST_IP = '.'.join(hostlist)
+HOST_PORT = 8855
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = '!DISCONNECT'
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.connect(ADDRESS)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+student_name = input('Enter your name: ')
 
 
-def send(msg: str):
-    msg = msg.encode(FORMAT)
-    msg_len = len(msg)
-    send_len = str(msg_len).encode(FORMAT)
-    send_len += b' ' * (HEADER - len(
-        send_len))  # ex len = 100, what we do is making a string that 64 byte long, 100 _ concat 63 blanks
-    print(f'[debugging] {send_len} ')
-    client_socket.send(send_len)
-    client_socket.send(msg)
+def udp_client():
+    while True:
+        message = f"{student_name}".encode(FORMAT)
+        client_socket.sendto(message, (HOST_IP, HOST_PORT))
+        print(f"{Colors.OKCYAN}[CLIENT]Sent broadcast message: {message.decode(FORMAT)}{Colors.ENDC}")
+        time.sleep(2)
 
 
 if __name__ == '__main__':
-
-    CLIENT_NAME = 'Faris Abufarha'
-    while True:
-        time.sleep(2)  # sleep 2 sec
-        send(f'{CLIENT_NAME}')
-
-        # if msg == DISCONNECT_MESSAGE:
-        #     break
+    udp_client()
--- Part2/server.py
+++ Part2/server.py
@@ -1,18 +1,18 @@
 import socket
-import threading
 import time
 from colors import Colors
 
+# no need to print the msg from the clien, just the reciever
 HOST_IP = socket.gethostbyname(socket.gethostname())  # my IP
 HOST_PORT = 8855
 ADDRESS = (HOST_IP, HOST_PORT)
 HEADER = 64  # HEADER represents 64 Bytes, maximum int size that the client will send
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = '!DISCONNECT'
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP SERVER
-server.bind(ADDRESS)
 
-last_received_msg = {}  # HashMap
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP SERVER
+server_socket.bind(ADDRESS)
+last_received_msg = {}  # Dictionary to store last received message from each client
 
 
 def get_current_time():
@@ -21,40 +21,27 @@ def get_current_time():
     current_time = time.strftime("%H:%M:%S", t)
     return current_time
 
-def handle_client(communication_socket, client_address):
-    received_time = get_current_time()
-    print(f'[NEW CONNECTION] {client_address} connected')
-    connected = True
-    while connected:
-        msg_length = communication_socket.recv(HEADER).decode(FORMAT)
-        if msg_length:  # not empty message
-            msg_length = int(msg_length)
-            msg = communication_socket.recv(msg_length).decode(FORMAT)
-            last_received_msg[client_address] = f'{msg} at {received_time}'
-            #communication_socket.send('[SERVER]: MESSAGE RECIEVED'.encode(FORMAT))
 
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-            print(f'[{client_address}]: {msg}')
-
-            if len(last_received_msg) >= 3: # if we have > 3 clients
-                print(f'[SERVER] OFN server ON')
-                for ip, msg in last_received_msg:
-                    print( ip, msg)
-
-
+def handle_client():
+    while True:
+        client_message, client_address = server_socket.recvfrom(1024)  # Receive message from client
+        received_time = get_current_time()
+        msg = client_message.decode(FORMAT)
+        last_received_msg[client_address] = f'{msg} at {received_time}'
+        print(f'[{client_address}]: {msg}')
+        if len(last_received_msg) >= 3:  # if we have > 3 clients
+            print(f'{Colors.OKBLUE}----------------------------------{Colors.ENDC}')
+            print(f'{Colors.OKGREEN}[SERVER] OFN server ON{Colors.ENDC}')
+            cnt = 1
+            for address, msg in last_received_msg.items():
+                print(f'{cnt}- received message from  {address} {msg}')
+                cnt += 1
+            print(f'{Colors.OKBLUE}----------------------------------{Colors.ENDC}')
 
 
 def start():
     print(f'{Colors.OKGREEN}[ONLINE] server is running on {HOST_IP} {Colors.ENDC}')
-    server.listen()
-    while True:
-        communication_socket, client_address = server.accept()
-        print(f'{communication_socket} {client_address}')
-        # thread = threading.Thread(target=handle_client, args=(communication_socket, address))
-        # thread.start()
-        handle_client(communication_socket, client_address)
-        print(f'[ACTIVE CONNECTIONS] {threading.activeCount() - 1}')
+    handle_client()
 
 
 if __name__ == '__main__':
