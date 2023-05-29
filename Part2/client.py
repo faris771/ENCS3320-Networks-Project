ce21494f54e73e1432e2cde6b326b2eeac9d102b
faris771 <farisabufahra33@gmail.com>
2023-05-29 08:18:16 +0300

    Output formatting edited  - always prints the updated messages not the just recieved or when they become 3
    + extra 255 octet handled

--- Part2/client.py
+++ Part2/client.py
@@ -4,6 +4,11 @@ from colors import Colors
 
 HOST_IP = socket.gethostbyname(socket.gethostname())  # my IP add 255 instead of last octet
 hostlist = HOST_IP.split('.')  # Split the IP address by '.'
+
+if hostlist[-3] == '255':
+    hostlist[-3] = '255'  # Change the  octet to 255
+if hostlist[-2] == '255':
+    hostlist[-2] = '255'  # Change the  octet to 255
 hostlist[-1] = '255'  # Change the last octet to 255
 HOST_IP = '.'.join(hostlist)  # Join the list again by '.'
 HOST_PORT = 8855  # Port that will be used by the server
@@ -11,8 +16,9 @@ FORMAT = 'utf-8'  # Format of the message that will be sent by the client
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create a socket for the client with IPv4 and UDP
 client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Enable broadcasting mode 1 is for True
-student_name = input('Enter your name: ')   # Get the name of the student from the user
 
+
+student_name = input('Enter your name: ')   # Get the name of the student from the user
 def udp_client(): # Function to handle the client
     while True:
         message = f"{student_name}".encode(FORMAT) # Encode the message to bytes
--- Part2/server.py
+++ Part2/server.py
@@ -2,43 +2,47 @@ import socket
 import time
 from colors import Colors
 
-# no need to print the msg from the clien, just the reciever
+# no need to print the msg from the client, just the receiver
 HOST_IP = socket.gethostbyname(socket.gethostname())  # my IP
-HOST_PORT = 8855 # Port that will be used by the server
-ADDRESS = (HOST_IP, HOST_PORT) # Address of the server
-FORMAT = 'utf-8'# Format of the message that will be sent by the client
+HOST_PORT = 8855  # Port that will be used by the server
+ADDRESS = (HOST_IP, HOST_PORT)  # Address of the server
+FORMAT = 'utf-8'  # Format of the message that will be sent by the client
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create a socket
-server_socket.bind(ADDRESS) # Bind the address to the socket
+server_socket.bind(ADDRESS)  # Bind the address to the socket
 last_received_msg = {}  # Dictionary to store last received message from each client
 
 
-def get_current_time(): # Function to get current time
+def get_current_time():  # Function to get current time
     # get current time
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     return current_time
 
 
-def handle_client(): # Function to handle clients
-    while True: # Loop to make the server running forever
+def handle_client():  # Function to handle clients
+    while True:  # Loop to make the server running forever
         client_message, client_address = server_socket.recvfrom(1024)  # Receive message from the client
-        received_time = get_current_time() # Get current time
-        msg = client_message.decode(FORMAT) # Decode the message to get a string
-        last_received_msg[client_address] = f'{msg} at {received_time}' # Store the client address with its last message
-        print(f'[{client_address}]: {msg}')
-        if len(last_received_msg) >= 3:  # if we have >= 3 clients
-            print(f'{Colors.OKBLUE}----------------------------------{Colors.ENDC}')
-            print(f'{Colors.OKGREEN}[SERVER] OFN server ON{Colors.ENDC}')
-            cnt = 1 # Counter to print the number of the client
-            for address, msg in last_received_msg.items():
-                print(f'{cnt}- received message from  {address} {msg}')
-                cnt += 1
-            print(f'{Colors.OKBLUE}----------------------------------{Colors.ENDC}')
+        received_time = get_current_time()  # Get current time
+        msg = client_message.decode(FORMAT)  # Decode the message to get a string
+        last_received_msg[
+            client_address] = f'{msg} at {received_time}'  # Store the client address with its last message
+
+        print(f'{Colors.OKBLUE}-{Colors.ENDC}'*10,end='')
+        server_msg = '[SERVER] UPDATED MESSAGES'
+        print(f'{Colors.OKGREEN}{server_msg}{Colors.ENDC}',end='')
+        print(f'{Colors.OKBLUE}-{Colors.ENDC}' * 10)
+
+        cnt = 1  # Counter to print the number of the client
+        for address, msg in last_received_msg.items():
+            print(f'{cnt}- received message from  {address} {msg}')
+            cnt += 1
+        print(f'{Colors.OKBLUE}-{Colors.ENDC}' * (20 + len(server_msg)))
+        print()
 
 
 def start():
-    print(f'{Colors.OKGREEN}[ONLINE] server is running on {HOST_IP} {Colors.ENDC}') # Print that the server is running
+    print(f'{Colors.OKGREEN}[ONLINE] server is running on {HOST_IP} {Colors.ENDC}')  # Print that the server is running
     handle_client()
 
 
